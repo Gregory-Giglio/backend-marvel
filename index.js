212eb32901a5ee6876edb450097b3e19e38291db
Gregory-Giglio <giglio.gregory@gmail.com>
2022-11-17 18:52:32 +0100

    paging feature added

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//NE PAS OUBLIER LE GITIGNORE !!!!!!!!!!!!
-
 
 app.get("/characters", async(req, res) =>{
     try {
@@ -19,17 +17,16 @@ app.get("/characters", async(req, res) =>{
 
         let skip=0;
         if (req.query.skip) {
-            limit = req.query.skip;
+            skip = req.query.skip;
         }
 
         let name="";
         if (req.query.name) {
-            limit = req.query.name;
+            name = req.query.name;
         }
-
         
         const response = await axios.get(
-            `https:\\https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&name=${name}`
+            `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&name=${name}`
         );
         res.json(response.data);
     } catch (error) {
@@ -37,10 +34,47 @@ app.get("/characters", async(req, res) =>{
     }
 });
 
+app.get("/comics", async(req, res) => {
+    try {
+        let limit=100;
+        if (req.query.limit) {
+            limit = req.query.limit;
+        }
+
+        let skip=0;
+        if (req.query.skip) {
+            skip = req.query.skip;
+        }
+
+        let title="";
+        if (req.query.title) {
+            title = req.query.title;
+        }
+        
+        const response = await axios.get(
+            `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&limit=${limit}&skip=${skip}&title=${title}`
+        )
+        res.json(response.data);
+    } catch (error) {
+        res.status(400).json({ message: error.message});
+    }
+});
+
+app.get("/comics/:characterId", async (req,res) => {
+    try {
+        const response = await axios.get(
+            `https://lereacteur-marvel-api.herokuapp.com/comics/${req.params.characterId}?apiKey=${process.env.MARVEL_API_KEY}`
+        );
+        res.json(response.data);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+});
+
 app.all("*", (req, res) => {
     res.status(404).json({ message: "Page not found"});
 });
 
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log("Server started");
 });
